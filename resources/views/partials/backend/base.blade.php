08f2e969d77167236362f3a1da38017fa3e6c1a8
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2023-04-14 14:07:54 +0700

    Template Admin

---
+++ resources/views/partials/backend/base.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="{{ asset('assets/img/fav.png') }}" type="image/x-icon">
+    <link rel="stylesheet" href="https://kit-pro.fontawesome.com/releases/v5.12.1/css/pro.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
+    <title>Welcome To Cleopatra</title>
+</head>
+
+<body class="bg-gray-100">
+
+
+    <!-- start navbar -->
+    @include('partials.backend.navbar')
+    <!-- end navbar -->
+
+
+    <!-- strat wrapper -->
+    <div class="h-screen flex flex-row flex-wrap">
+
+        <!-- start sidebar -->
+        @include('partials.backend.sidebar')
+        <!-- end sidbar -->
+
+        <!-- strat content -->
+        <div class="bg-gray-100 flex-1 p-6 md:mt-16">
+
+
+            <!-- General Report -->
+            <div class="grid grid-cols-4 gap-6 xl:grid-cols-1">
+
+
+                <!-- card -->
+                {{-- @include('partials.backend.card') --}}
+                <!-- end card -->
+                <div class="konten">
+                    @yield('konten')
+                </div>
+
+            </div>
+            <!-- End General Report -->
+        </div>
+        <!-- end content -->
+
+    </div>
+    <!-- end wrapper -->
+
+    <!-- script -->
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    <script src="{{ asset('assets/js/scripts.js') }}"></script>
+    <!-- end script -->
+
+</body>
+
+</html>
