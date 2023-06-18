4a64238b3cbe0c135c28f56716873c8ec4cab42e
ZeroneDoo <rizqipambudi6@gmail.com>
2023-06-18 11:19:28 +0700

    Add Homepage

--- resources/views/frontend/Homepage.blade.php
+++ resources/views/frontend/Homepage.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 <div class="px-8 py-10 grid grid-cols-4 gap-6">
     <div class="card w-64 bg-base-100 shadow-md">
-        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+        <figure><img src="{{asset('images/img.png')}}" alt="Shoes" /></figure>
         <div class="card-body">
             <h2 class="card-title">Shoes!</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="flex items-center">
+            <div class="flex items-center flex-col gap-3 justify-between">
                 <p class="font-bold">Rp.1.000.000</p>
-                <button class="btn btn-primary text-white">Add To Chart</button>
+                <button class="btn btn-primary dark:bg-gray-600 text-white">Add To Chart</button>
             </div>
             {{-- <div class="card-actions justify-end mt-5 flex ">
                 <button class="btn btn-primary text-white">Buy Now</button>
